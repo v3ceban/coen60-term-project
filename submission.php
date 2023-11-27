7febebcab32a42c00ee734898849ebb55b7f6fd0
Vladimir Ceban <v3ceban@proton.me>
2023-11-27 10:34:28 -0800

    term

--- submission.php
+++ submission.php
@@ -6,7 +6,16 @@
   </head>
   <body>
     <?php include "./parts/header.php" ?>
-    <?php $form_data = $_POST ?>
+    <?php
+    $form_data = $_POST;
+
+    $email = $form_data['Email'];
+    $domain = explode('@', $email)[1];
+
+    if ($domain !== 'scu.edu') {
+        echo "Email must be from scu.edu domain";
+    }
+    ?>
     <main class="submission">
       <h2>Your Data</h2>
       <?php foreach ($form_data as $key => $value) {
