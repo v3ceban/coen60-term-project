52bf94233b9383c34105fcb5a557292f17589de9
Vladimir Ceban <v3ceban@proton.me>
2023-11-28 13:07:37 -0800

    integrating archives into the page

--- archive.php
+++ archive.php
@@ -2,11 +2,20 @@
 <html lang="en">
   <head>
     <?php include "./parts/head-content.php"?>
+    <link rel="stylesheet" href="./css/pages/archive.css">
     <title>Archive</title>
   </head>
   <body>
     <?php include "./parts/header.php" ?>
-    <main class="archive"></main>
+    <main class="archive">
+      <div class="content">
+        <h2>From the Archives</h2>
+        <p>text about the page</p>
+        <h3>2023 Projects</h3>
+        <h4>Award Winners</h4>
+      </div>
+      <?php include "./parts/sidenav.php"?>
+    </main>
     <?php include "./parts/footer.php" ?>
   </body>
-</html>
+</php>
