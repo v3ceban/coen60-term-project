3837e79e94bd8b13f3eccaeb251ed07ec5386a47
miaadele <123035906+miaadele@users.noreply.github.com>
2023-11-29 00:48:56 -0800

    Update archive.php
    Updated 1am on Nov 29

--- archive.php
+++ archive.php
@@ -8,14 +8,72 @@
   <body>
     <?php include "./parts/header.php" ?>
     <main class="archive">
-      <div class="content">
-        <h2>From the Archives</h2>
-        <p>text about the page</p>
-        <h3>2023 Projects</h3>
-        <h4>Award Winners</h4>
+    <div class="content">
+        <!--Sidebar navigation-->
+            <div class="sidenav">
+                <h3 style="padding-left: 6px;">Archives</h3>
+                <a href="archive22.html">2022</a>
+                <a href="archive21.html">2021</a>
+                <a href="archive20.html">2020</a>
+                <a href="archive19.html">2019</a>
+                <a href="archive18.html">2018</a>
+                <a href="archive17.html">2017</a>
+                <a href="archive16.html">2016</a>
+                <a href="archive15.html">2015</a>
+                <a href="archive14.html">2014</a>
+                <a href="archive13.html">2013</a>
+                <a href="archive12.html">2012</a>
+            </div>
+
+        <h1>Explore Past Senior Design Projects</h1>
+        <p class="main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.</p>
+        <div class="jumpLinks"> 
+            <a href="bioe.html">Bioengineering</a> 
+            <a href="ceng.html">Civil Engineering</a> 
+            <a href="coen.html">Computer Engineering</a> 
+            <a href="elen.html">Electrical Engineering</a> 
+            <a href="mech.html">Mechanical Engineering</a> 
+            <a href="idsc.html">Interdisciplinary</a> 
+        </div>
+        <p class="main">At vero eos et accusamus et iusto odio dignissimos ducimus 
+            qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores 
+            et quas molestias excepturi sint occaecati cupiditate non provident, 
+            similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. 
+            Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, 
+            cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, 
+            omnis voluptas assumenda est, omnis dolor repellendus. 
+            Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut 
+            et voluptates repudiandae sint et molestiae non recusandae. 
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias 
+            consequatur aut perferendis doloribus asperiores repellat.</p>
+        <h1 class="main" style="text-align: center;background-color: #b0aa7e; padding: 5px;"><a href="archive23.html">2023 Projects</a></h1>
+        <div class="current">
+            <p>
+            <h3>View highlights from the 2023 Senior Design Conference.</h3>
+            <iframe height="360" width="600" src="https://www.youtube.com/embed/1DBi0eK2g7I?si=qb8bPeRC_kbQI4Tg" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+            clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen>
+            </iframe>
+        </div>
+        
+        <div class="current">
+        <h3>Click the image below to access the 2023 Senior Design Gallery.</h3>
+        <p><a title="2023 Senior Design Photo Gallery" href="https://scu.canto.com/v/2023SeniorDesignConference/s/MICRK?viewIndex=0&amp;gSortingForward" 
+            target="_blank" rel="noopener"><img src="Photos/2023Gallery.JPG" 
+            alt="A group of seniors showcase their project in the SCDI courtyard. Title overlay reads 2023 Senior Design." 
+            height="360"></a>
+        </p>
+        </div>
       </div>
-      <?php include "./parts/sidenav.php"?>
-    </main>
+    <?php include "./parts/sidenav.php"?>
+  </main>
     <?php include "./parts/footer.php" ?>
-  </body>
+    </body>
 </php>
