1c6d303610536fa3f10805cfde6380987a01e2ea
xinyujieHong <xinyujie.hong@gmail.com>
2023-11-28 21:41:20 -0800

    fix the header link

--- parts/header.php
+++ parts/header.php
@@ -1,12 +1,12 @@
 <header>
-  <h1><a href="../index.php">Senior Design Finder</a></h1>
+  <h1><a href="./index.php">Senior Design Finder</a></h1>
   <nav>
     <ul>
-      <li><a href="../index.php">Home</a></li>
-      <li><a href="../about.php">About</a></li>
-      <li><a href="../archive.php">Archive</a></li>
-      <li><a href="../currentTopics.php">People</a></li>
-      <li><a href="../form.php">Form</a></li>
+      <li><a href="./index.php">Home</a></li>
+      <li><a href="./about.php">About</a></li>
+      <li><a href="./archive.php">Archive</a></li>
+      <li><a href="./currentTopics.php">People</a></li>
+      <li><a href="./form.php">Form</a></li>
     </ul>
   </nav>
 </header>
