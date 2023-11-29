75c14828d2058b0350bb62b666f2947fd3f7edfe
Vladimir Ceban <v3ceban@proton.me>
2023-11-28 16:26:05 -0800

    form collection

--- submission.php
+++ submission.php
@@ -6,21 +6,22 @@
   </head>
   <body>
     <?php include "./parts/header.php" ?>
-    <?php
-    $form_data = $_POST;
-
-    $email = $form_data['Email'];
-    $domain = explode('@', $email)[1];
-
-    if ($domain !== 'scu.edu') {
-        echo "Email must be from scu.edu domain";
-    }
-    ?>
     <main class="submission">
       <h2>Your Data</h2>
-      <?php foreach ($form_data as $key => $value) {
-          echo "<p>$key: $value</p>";
-      } ?>
+    <?php
+    $files = glob("./submissions/*");
+    $id = (int) pathinfo(end($files), PATHINFO_FILENAME);
+    $newId = $id + 1;
+    $file = fopen("./submissions/$newId.txt", "w");
+    foreach ($_POST as $key => $value) {
+        ${$key} = $value;
+        fwrite($file, "$key: $value\n");
+    }
+    fclose($file);
+    $firstName = explode(" ", $Name)[0];
+    $lastName = explode(" ", $Name)[1];
+    echo "<p>Thank you ".ucwords($firstName).", your form has been submitted.</p>";
+    ?>
     </main>
     <?php include "./parts/footer.php" ?>
   </body>
