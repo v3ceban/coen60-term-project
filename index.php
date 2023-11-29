52ef338d1431173e48a7bfde67dccc7b9da6f9f5
hoholly <hengyu1112@gmail.com>
2023-11-28 17:42:27 -0800

    update of homeage

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
+
 <html lang="en">
-  <head>
-    <?php include "./parts/head-content.php" ?>
-    <title>Home</title>
-  </head>
-  <body>
-    <?php include "./parts/header.php" ?>
-    <main class="home"></main>
-    <?php include "./parts/footer.php" ?>
-  </body>
+
+<head>
+
+  <?php include "./parts/head-content.php" ?>
+  <link rel="stylesheet" href="home.css">
+
+  <title>Home</title>
+
+</head>
+
+<body>
+
+  <?php include "./parts/header.php" ?>
+
+
+  <div class="image-container">
+
+    <img src="background1.jpg" alt="Background Image">
+
+    <h1 class="image-text"> Explore Ideas & Find Your Teammates </h1>
+
+  </div>
+
+
+  <div class="intro">
+
+    <p class="para1">Every senior in the School of Engineering will team up and create a year-long capstone project to showcase their learning by solving real-life problems.</p>
+
+    <p>However, coming up with ideas and forming a team might be challenging. This website will allow you to get inspiration from previous designs and connect with your peers.</p>
+
+  </div>
+
+
+  <div class="buttons">
+
+    <a href=""><button type="button" class="archive">See Archives</button></a>
+
+    <a href=""><button type="button" class="form">Find Teammates</button></a>
+
+    <a href=""><button type="button" class="about">About Us</button></a>
+
+  </div>
+
+  <?php include "./parts/footer.php" ?>
+
+</body>
+
 </html>
