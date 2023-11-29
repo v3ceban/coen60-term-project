ff57e2c919ccd51ce4fe3d1324fbc9a91ad1baad
Vladimir Ceban <v3ceban@proton.me>
2023-11-29 12:37:55 -0800

    renamed currentTopics to people

--- parts/header-archive.php
+++ parts/header-archive.php
@@ -5,7 +5,7 @@
       <li><a href="../index.php">Home</a></li>
       <li><a href="../about.php">About</a></li>
       <li><a href="../archive.php">Archive</a></li>
-      <li><a href="../currentTopics.php">People</a></li>
+      <li><a href="../people.php">People</a></li>
       <li><a href="../form.php">Form</a></li>
     </ul>
   </nav>
