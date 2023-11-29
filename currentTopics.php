5006274391290fd516aaac399955f484a2442884
xinyujieHong <xinyujie.hong@gmail.com>
2023-11-28 18:00:29 -0800

    current topics

---
+++ currentTopics.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Current Topics</title>
+	<link rel="stylesheet" type="text/css" href="collection.css">
+	<style>
+		body{
+		background-color: #f4f2f0;
+	}
+	</style>
+</head>
+<body>
+	<!--header-->
+	<div class="head">
+		<span class="part"><a href="coen60-term-project/index.php">Home</a></span>
+		<span class="part"><a href="aboutPage.php">About</a></span>
+		<span class="part"><a href="coen60-term-project/form.php">Form</a></span>
+		<span class="part"><a href="coen60-term-project/archive.php">Archive</a></span>
+		<span class="part"><a href="collection.php">Current Topics</a></span>
+	</div>
+
+	<!--submission-->
+	
+
+</body>
+</html>
