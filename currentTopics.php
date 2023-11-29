d9d62144a9260fd4058d71b54ff450dd4d8d3ce5
xinyujieHong <xinyujie.hong@gmail.com>
2023-11-28 18:38:51 -0800

    change

--- currentTopics.php
+++ currentTopics.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Current Topics</title>
+	<?php include "./parts/head-content.php"?>
 	<link rel="stylesheet" type="text/css" href="coen60-term-project/css/pages/currentTopics.css">
 	<style>
 		body{
@@ -11,13 +12,7 @@
 </head>
 <body>
 	<!--header-->
-	<div class="head">
-		<span class="part"><a href="index.php">Home</a></span>
-		<span class="part"><a href="about.php">About</a></span>
-		<span class="part"><a href="form.php">Form</a></span>
-		<span class="part"><a href="archive.php">Archive</a></span>
-		<span class="part"><a href="currentTopics.php">Current Topics</a></span>
-	</div>
+	<?php include "./parts/header.php" ?>
 
 	<!--submission-->
 
