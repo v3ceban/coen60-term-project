4bb4df621692d3f759a13e63a04e3954058984f7
Vladimir Ceban <v3ceban@proton.me>
2023-11-28 17:19:02 -0800

    form collection

--- submission.php
+++ submission.php
@@ -7,12 +7,11 @@
   <body>
     <?php include "./parts/header.php" ?>
     <main class="submission">
-      <h2>Your Data</h2>
+      <h2>Your form was submitted</h2>
     <?php
     $files = glob("./submissions/*");
-    $id = (int) pathinfo(end($files), PATHINFO_FILENAME);
-    $newId = $id + 1;
-    $file = fopen("./submissions/$newId.txt", "w");
+    $id = count($files) + 1;
+    $file = fopen("./submissions/$id.txt", "w");
     foreach ($_POST as $key => $value) {
         ${$key} = $value;
         fwrite($file, "$key: $value\n");
@@ -21,6 +20,32 @@
     $firstName = explode(" ", $Name)[0];
     $lastName = explode(" ", $Name)[1];
     echo "<p>Thank you ".ucwords($firstName).", your form has been submitted.</p>";
+    ?>
+      <h2>Who you can connect with</h2>
+    <?php
+    $files = scandir("./submissions/");
+    $matchingFiles = [];
+    foreach ($files as $file) {
+        if (is_file("./submissions/" . $file)) {
+            $content = file_get_contents("./submissions/" . $file);
+            if (strpos($content, $Major) !== false) {
+                array_push($matchingFiles, $file);
+            }
+        }
+    }
+    $randomFiles = array_rand($matchingFiles, 3);
+    $i = 1;
+    foreach ($randomFiles as $index) {
+        $content = file_get_contents("./submissions/" . $matchingFiles[$index]);
+        $lines = explode("\n", $content);
+        echo("<h3>Connection #$i</h3>");
+        foreach ($lines as $line) {
+            if (strpos($line, "Name:") === 0 || strpos($line, "Email:") === 0) {
+                echo nl2br($line."\n");
+            }
+        }
+        $i++;
+    }
     ?>
     </main>
     <?php include "./parts/footer.php" ?>
